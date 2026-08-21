74007362f1c1cbdfd20d533d330cdf41d64922b2
Ankit <ankit27522@gmail.com>
2026-08-21 15:30:17 +0530

    uploaded the link of git repository

--- src/components/Bottom.jsx
+++ src/components/Bottom.jsx
@@ -11,7 +11,7 @@ const Bottom = () => {
       <div className='center text-white my-auto'>Made with ❤️ by Ankit</div>
       <div className='right flex'>
           <img src={wizard} alt="" className='h-15'/>
-          <a href="https://github.com/ankit27522-hash/" target="_blank" > 
+          <a href="https://github.com/ankit27522-hash/Elf-s-Vault" target="_blank" > 
           <button className='text-white bg-green-700 my-auto mx-2 rounded-full flex items-center ring-white ring-1 cursor-pointer'> 
               <img className=' w-10 p-1' src={gitlogo} alt="github logo" />
               <span className='font-bold px-2'>GitHub</span>
